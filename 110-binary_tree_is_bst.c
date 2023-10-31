290bd7f9d82c193c69583e10f32f6caf8dd34efc
Ayomide Olisa <lekelisa247@gmail.com>
2023-10-31 05:30:27 -0700

    update on binary tree project

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -1,121 +1,36 @@
 #include "binary_trees.h"
-#include "limits.h"
-int is_bst_helper(const binary_tree_t *tree, int *largest);
-
-#define VERBOSE 0
-
 /**
- * binary_tree_is_bst - checks if a binary tree is a valid Binary Search Tree.
- * @tree: A pointer to the root node of the tree to check.
- *
- * Return: 1 if tree is a valid BST, and 0 otherwise
+ * isBST - check if each node has in the left a less value than himself
+ * @root: node to check
+ * @min: min value to check with the node at the left
+ * @max: max value to check with the node at the right
+ * Return: 1 if is a BST or 0 otherwise
+ */
+int isBST(const binary_tree_t *root,
+	   const binary_tree_t *min, const binary_tree_t *max)
+{
+	if (root == NULL)
+		return (1);
+
+	if (min != NULL && root->n <= min->n)
+		return (0);
+
+	if (max != NULL && root->n >= max->n)
+		return (0);
+
+	return (isBST(root->left, min, root) &&
+		isBST(root->right, root, max));
+}
+/**
+ * binary_tree_is_bst - checks if a binary tree is a nid Binary Search Tree
+ * @tree: a pointer to the node node of the tree to check
+ * Return: 1 if tree is a nid BST, and 0 otherwise
+ * If tree is NULL, return 0
  */
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
-	int tracker = INT_MIN;
-
-	if (tree == NULL)
+	if (!tree)
 		return (0);
-	return (is_bst_helper(tree, &tracker));
-}
 
-
-/**
- * is_left - checks if a node is a left child of some other node
- * @node: A pointer to the node to be checked.
- * Return: 1 if is a left child, otherwise 0
- */
-int is_left(const binary_tree_t *node)
-{
-	if (node && node->parent)
-		return (node->parent->left == node);
-	return (0);
+	return (isBST(tree, NULL, NULL));
 }
-/**
- * is_right - checks if a node is a right child of some other node
- * @node: A pointer to the node to be checked.
- * Return: 1 if is a right child, otherwise 0
- */
-int is_right(const binary_tree_t *node)
-{
-	if (node && node->parent)
-		return (node->parent->right == node);
-	return (0);
-}
-
-#if !VERBOSE
-/**
- * is_bst_helper - checks if a binary tree is a valid Binary Search Tree.
- * @tree: A pointer to the root node of the tree to check.
- * @largest: Value of largest node visited so far.
- *
- * Return: 1 if tree is a valid BST, and 0 otherwise
- */
-int is_bst_helper(const binary_tree_t *tree, int *largest)
-{
-	int ret = 1;
-
-	if (tree != NULL)
-	{
-		ret *= is_bst_helper(tree->left, largest);
-		if (tree->n < *largest)
-			return (0);
-		*largest = tree->n;
-		if (is_left(tree) && !(tree->n < tree->parent->n))
-			return (0);
-		if (is_right(tree) && !(tree->n > tree->parent->n))
-			return (0);
-		ret *= is_bst_helper(tree->right, largest);
-	}
-	return (ret);
-}
-#else
-/**
- * is_bst_helper - checks if a binary tree is a valid Binary Search Tree.
- * @tree: A pointer to the root node of the tree to check.
- * @largest: Value of largest node visited so far.
- *
- * Return: 1 if tree is a valid BST, and 0 otherwise
- */
-int is_bst_helper(const binary_tree_t *tree, int *largest)
-{
-	int ret = 1;
-
-	if (tree)
-	{
-		printf("Moving to %d\n", tree->n);
-		ret *= is_bst_helper(tree->left, largest);
-		printf("done with left tree for %d: %d\n", tree->n, ret);
-		printf("largest = %d\n", *largest);
-		if (tree->n < *largest)
-			return (0);
-		*largest = tree->n;
-		printf("largest = %d\n", *largest);
-		if (is_left(tree))
-			printf("%d is %s than %d\n",
-				tree->n,
-				(tree->n < tree->parent->n) ? "smaller" : "larger",
-				tree->parent->n);
-		if (is_left(tree) && !(tree->n < tree->parent->n))
-		{
-			printf("%d is left child\n", tree->n);
-			return (0);
-		}
-		if (is_right(tree))
-			printf("%d is %s than %d\n",
-				tree->n,
-				(tree->n < tree->parent->n) ? "smaller" : "larger",
-				tree->parent->n);
-		if (is_right(tree) && !(tree->n > tree->parent->n))
-		{
-			printf("%d is right child\n", tree->n);
-			return (0);
-		}
-		ret *= is_bst_helper(tree->right, largest);
-		printf("done with right tree for %d: %d\n", tree->n, ret);
-		if (tree->parent)
-			printf("Moving back to %d\n", tree->parent->n);
-	}
-	return (ret);
-}
-#endif /* VERBOSE */
